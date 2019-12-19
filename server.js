cf522e08aafd5b175b462aafb2cd004d8dcd756b
dang lu <danglu@dangs-mbp.lan>
2019-12-18 17:11:02 -0800

    mvp reached

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const accountsRouter = require('./routers/accounts')
 
 const server = express();
 
 server.use(express.json());
+
+server.get('/', (req,res)=>{
+    res.send('welcome')
+})
+
+server.use('/api/accounts', accountsRouter)
+
+server.use((err, req, res, next)=>{
+    console.log(err)
+    res.status(500).json({message:'something went wrong'})
+})
+
+
 
 module.exports = server;
